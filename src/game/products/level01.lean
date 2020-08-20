258016d6c11626401ce3a150f73dcdf566dc7626
Dan Stanescu <stanescu@uwyo.edu>
2020-08-20 14:48:45 -0600

    Update

---
+++ src/game/products/level01.lean
@@ -0,0 +1,30 @@
+import tactic
+import data.rat
+import data.real.basic
+import data.real.irrational
+import data.zmod.basic
+import game.basic.level02
+
+namespace uwyo -- hide
+open real -- hide
+
+/-
+# Chapter 3 : Products
+
+## Level 1
+
+-/
+
+/- Lemma
+The product of a rational and an irrational number may be a rational.
+-/
+theorem irrat_prod : ¬ (∀ (a : ℝ), ∀ (b : ℚ), irrational a → irrational (a*b)) :=
+begin
+    intro H,
+    have H1 := H (sqrt 2) (0: ℚ),
+    have H2 := H1 irrational_sqrt_two,
+    apply H2,
+    existsi (0 : ℚ), simp, done
+end
+
+end uwyo -- hide
--- src/game/sums/level01.lean
+++ src/game/sums/level01.lean
@@ -1,45 +1,32 @@
-import data.nat.prime
-import data.rat.basic
-import data.real.basic
 import tactic
+import data.rat
+import data.real.basic
+import data.real.irrational
+import data.zmod.basic
+import game.basic.level02
 
--- Try to remove the three `sorry` in the last lemma below
--- You may need to do it on paper first
 namespace uwyo -- hide
+open real -- hide
+
 
 /-
-# Chapter 1 : Basic facts
+# Chapter 2 : Sums
 
 ## Level 1
 
-In this level you need to prove that `sqrt 2` is irrational.
-This is adapted from a similar proof from chapter 2 of "logic and Proof"
 -/
-
 
 /- Lemma
-For any two arbitrary natural numbers $a$ and $b$, it is not true that
-$$ a^2 = 2 b^2$$.
+There are irrational numbers that sum up to a rational.
 -/
-theorem sqrt_two_irrational {a b : ℕ} (co : nat.gcd a b = 1) : a^2 ≠ 2 * b^2 :=
+theorem irrat_sum : ¬ (∀ (x y : ℝ), irrational x → irrational y → irrational (x+y)) :=
 begin
-    intro h,
-    have h1 : 2 ∣ a^2, simp [h],
-    have h2 : 2 ∣ a, from nat.prime.dvd_of_dvd_pow nat.prime_two h1,
-    cases h2 with c aeq,
-    have h3 : 2 * ( 2 * c^2) = 2 * b^2,
-        by simp [eq.symm h, aeq]; 
-           simp [nat.pow_succ, mul_comm, mul_assoc, mul_left_comm],
-    have h4 : 2 * c^2 = b^2,
-        from nat.eq_of_mul_eq_mul_left dec_trivial h3,
-    have h5 : 2 ∣ b^2, by simp [eq.symm h4],
-    have hb : 2 ∣ b, from nat.prime.dvd_of_dvd_pow nat.prime_two h5,
-    have ha : 2 ∣ a, from nat.prime.dvd_of_dvd_pow nat.prime_two h1,
-    have h6 : 2 ∣ nat.gcd a b, from nat.dvd_gcd ha hb,
-    have habs : 2 ∣ (1 : ℕ), by 
-        {rw co at h6, exact h6},
-    have h7 : ¬ 2 ∣ 1, exact dec_trivial,
-    exact h7 habs, done
+  intro H,
+  have H2 := H (sqrt 2) (-sqrt 2),
+  have H3 := H2 irrational_sqrt_two (irrational.neg irrational_sqrt_two),
+  apply H3,
+  existsi (0 : ℚ),
+  simp, done
 end
 
 end uwyo -- hide
