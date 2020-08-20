60bc6ae23ef67f17ca4cc59586694ddac6498090
Dan Stanescu <stanescu@uwyo.edu>
2020-08-20 13:41:04 -0600

    Initial setup

---
+++ src/game/basic/level02.lean
@@ -0,0 +1,45 @@
+import data.nat.prime
+import data.rat.basic
+import data.real.basic
+import tactic
+
+-- Try to remove the three `sorry` in the last lemma below
+-- You may need to do it on paper first
+namespace uwyo -- hide
+
+/-
+# Chapter 1 : Basic facts
+
+## Level 1
+
+In this level you need to prove that `sqrt 2` is irrational.
+This is adapted from a similar proof from chapter 2 of "logic and Proof"
+-/
+
+
+/- Lemma
+For any two arbitrary natural numbers $a$ and $b$, it is not true that
+$$ a^2 = 2 b^2$$.
+-/
+theorem sqrt_two_irrational {a b : ℕ} (co : nat.gcd a b = 1) : a^2 ≠ 2 * b^2 :=
+begin
+    intro h,
+    have h1 : 2 ∣ a^2, simp [h],
+    have h2 : 2 ∣ a, from nat.prime.dvd_of_dvd_pow nat.prime_two h1,
+    cases h2 with c aeq,
+    have h3 : 2 * ( 2 * c^2) = 2 * b^2,
+        by simp [eq.symm h, aeq]; 
+           simp [nat.pow_succ, mul_comm, mul_assoc, mul_left_comm],
+    have h4 : 2 * c^2 = b^2,
+        from nat.eq_of_mul_eq_mul_left dec_trivial h3,
+    have h5 : 2 ∣ b^2, by simp [eq.symm h4],
+    have hb : 2 ∣ b, from nat.prime.dvd_of_dvd_pow nat.prime_two h5,
+    have ha : 2 ∣ a, from nat.prime.dvd_of_dvd_pow nat.prime_two h1,
+    have h6 : 2 ∣ nat.gcd a b, from nat.dvd_gcd ha hb,
+    have habs : 2 ∣ (1 : ℕ), by 
+        {rw co at h6, exact h6},
+    have h7 : ¬ 2 ∣ 1, exact dec_trivial,
+    exact h7 habs, done
+end
+
+end uwyo -- hide
---
+++ src/game/intro.lean
@@ -0,0 +1,34 @@
+/-
+# The Rational Number Game, version 0.1beta
+
+## By Dan Stanescu 
+
+# What is this game?
+
+Welcome to the rational number game -- a game to help undergraduates learn analysis through Lean,
+a formal proof verification system. The game explores some basic facts about the rationals, 
+starting with the fact that the square root of two is not a rational number.
+
+This game is a sequel to
+<a href="http://wwwf.imperial.ac.uk/~buzzard/xena/natural_number_game/" target="blank">the natural number game</a>.
+The levels in the Rational Number Game need to be solved using tactics. To learn how to use these tactics, I would
+recommend that you first play the Natural Number Game up to at least "Advanced Proposition world". I will
+not go through a careful explanation of the tactics taught by the natural number game here.
+
+Blue nodes in the graph on the right are ones that you are ready to enter. Gray nodes you should stay away
+from -- try blue ones higher up the chain first. Green nodes are completed.
+
+# Thanks
+
+Many thanks to both Kevin Buzzard and Mohammad Pedramfar, without whom this game would not exist.
+
+# Questions?
+
+You can ask questions on the <a href="https://leanprover.zulipchat.com/" target="blank">Lean Zulip chat</a>,
+where I am often to be found. You can also ask me questions if you take a class with me, or bump into me somewhere
+on the UWyo campus.
+
+The Rational Number Game is brought to you by the UWyo Proof Games project.
+
+Prove a theorem. Write a function. <a href="https://twitter.com/XenaProject" target="blank">@XenaProject</a>.
+-/
